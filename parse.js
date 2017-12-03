d13bae73a6005644c9c3377f26d2b96058699e69
Kalan <kjj6198@gmail.com>
2017-12-03 22:38:18 +0800

    add mutiple props.

--- parse.js
+++ parse.js
@@ -12,7 +12,7 @@ function parse(text) {
 
   function next(c) {
     if (c && ch !== c) {
-      throw SyntaxError('');
+      throw SyntaxError(`expected current char was \`${c}\`, but got ${ch}`);
     }
 
     position += 1;
@@ -61,15 +61,18 @@ function parse(text) {
       }
       while (ch) {
         key = string();
-
         skip();
         next(':');
-        object[key] = number();
+        object[key] = value();
         skip();
         if (ch === '}') {
-          next(',');
+          next('}');
+
           return object;
         }
+
+        next(',');
+        skip();
       }
 
     }
@@ -89,13 +92,29 @@ function parse(text) {
       string += '.';
       while (next() && ch >= '0' && ch <= '9') {
         string += ch;
+        next();
       }
     }
 
     return parseInt(string, 10);
   }
+
+  function value() {
+    skip();
+    switch(ch) {
+      case '{':
+        return object();
+      case '"':
+        return string();
+      case '-':
+        return number();
+      default:
+        return ch >= '0' && ch <= '9' ? number() : null;
+    }
+  }
+
   ch = text.charAt(0);
-  let result = object();
+  let result = value();
   return result;
 }
 
