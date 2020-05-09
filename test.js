fe6cef0a4bd11ef7b3dc38cf91108286fc0e9234
kjj6198 <kjj6198@gmail.com>
2020-05-09 21:53:07 +0900

    add test

---
+++ test.js
@@ -0,0 +1,37 @@
+const assert = require("assert");
+const fs = require("fs");
+const Parser = require("./parse");
+
+describe("JSON parser", () => {
+  const dirs = fs.readdirSync("test");
+  dirs.forEach((dir) => {
+    if (dir === ".") {
+      return;
+    }
+
+    const sample = fs.readFileSync(`test/${dir}/sample.json`);
+
+    if (fs.existsSync(`test/${dir}/config.json`)) {
+      it(dir, () => {
+        const config = JSON.parse(
+          fs
+            .readFileSync(`test/${dir}/config.json`, { encoding: "utf8" })
+            .toString()
+        );
+        assert.throws(() => {
+          const actual = new Parser(sample.toString()).parse();
+        }, config.message);
+      });
+    } else {
+      const expected = fs
+        .readFileSync(`test/${dir}/expect.json`, {
+          encoding: "utf8",
+        })
+        .toString();
+      const actual = new Parser(sample.toString()).parse();
+      it(dir, () => {
+        assert.deepEqual(actual, JSON.parse(expected));
+      });
+    }
+  });
+});
