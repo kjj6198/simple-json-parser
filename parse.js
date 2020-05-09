5ebd427c9b6df6f4fa30037c9e04755dc372979f
kjj6198 <kjj6198@gmail.com>
2020-05-09 21:52:21 +0900

    added structural JSON parser

--- parse.js
+++ parse.js
@@ -1,3 +1,5 @@
+const object = require("./state/object");
+
 const escape = {
   '"': '"',
   t: "\t",
@@ -5,170 +7,48 @@ const escape = {
   "\\": "\\",
 };
 
-function parse(text) {
-  let ch = " ";
-  let position = 0;
-
-  function next(c) {
-    if (c && ch !== c) {
-      throw SyntaxError(
-        `expected current char was \`${c}\`, but got ${ch} at position ${position}`
-      );
-    }
-
-    position += 1;
-    ch = text.charAt(position);
-    return ch;
+class Parser {
+  constructor(raw) {
+    this.raw = raw;
+    // position
+    this.index = 0;
   }
 
-  function skip() {
-    while (ch <= " ") {
-      position += 1;
-      ch = text.charAt(position);
-    }
+  remaining() {
+    return this.raw.slice(this.index);
   }
 
-  function string() {
-    let string = "";
-    skip();
-
-    if (ch === '"') {
-      while (next()) {
-        if (ch === '"') {
-          next(); // manually next char.
-          return string;
-        } else if (ch === "\\") {
-          next();
-          if (escape[ch]) {
-            string += escape[ch];
-          }
-        } else {
-          string += ch;
-        }
-      }
-    }
-
-    throw new SyntaxError(
-      "Unexpected token while parsing string at position " + position
-    );
+  current() {
+    return this.raw[this.index];
   }
 
-  function object() {
-    let key = "";
-    const object = {};
-    if (ch === "{") {
-      next("{");
-      skip();
+  // match given str
+  next(str) {
+    if (this.raw.slice(this.index, this.index + str.length) === str) {
+      this.index += str.length;
+      return true;
+    }
 
-      if (ch === "}") {
-        next("}");
-        return Object.create(null);
-      }
+    return false;
+  }
 
-      while (ch) {
-        key = string();
-        skip();
-        next(":");
-        object[key] = value();
-        skip();
-        if (ch === "}") {
-          next("}");
-
-          return object;
-        }
-
-        next(",");
-        skip();
-      }
+  skip() {
+    while (this.raw[this.index] <= " ") {
+      this.index += 1;
     }
   }
 
-  function array() {
-    const array = [];
-    if (ch === "[") {
-      next("[");
-      skip();
-
-      if (ch === "]") {
-        // terminate
-        next("]");
-        return array;
-      }
-
-      while (ch) {
-        array.push(value());
-        skip();
-        console.log(ch);
-
-        if (ch === "]") {
-          next("]");
-          return array;
-        }
-        next(",");
-        skip();
-      }
+  read(pattern) {
+    const match = pattern.exec(this.raw.slice(this.index));
+    if (!match || match.index !== 0) {
+      return null;
     }
   }
 
-  function keyword() {
-    const keywords = ["true", "false", "null"];
-    switch (ch) {
-      case "t":
-        keywords[0].split("").forEach((char) => next(char));
-        return true;
-      case "f":
-        keywords[1].split("").forEach((char) => next(char));
-        return false;
-      case "n":
-        keywords[2].split("").forEach((char) => next(char));
-        return null;
-      default:
-        throw new SyntaxError("unexpected token at position " + position);
-    }
+  parse() {
+    const value = object(this);
+    return value;
   }
-
-  function number() {
-    let string = "";
-
-    if (ch === "-") {
-      string = "-";
-    }
-    while (ch >= "0" && ch <= "9") {
-      string += ch;
-      next();
-    }
-
-    if (ch === ".") {
-      string += ".";
-      while (next() && ch >= "0" && ch <= "9") {
-        string += ch;
-        next();
-      }
-    }
-
-    return parseInt(string, 10);
-  }
-
-  function value() {
-    skip();
-    switch (ch) {
-      case "{":
-        return object();
-      case '"':
-        return string();
-      case "-":
-        return number();
-      case "[":
-        console.log("array");
-        return array();
-      default:
-        return ch >= "0" && ch <= "9" ? number() : keyword();
-    }
-  }
-
-  ch = text.charAt(0);
-  let result = value();
-  return result;
 }
 
-console.log(parse('{"123": [1,adf   2, 3]}'));
+module.exports = Parser;
